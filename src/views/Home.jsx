d661e7f98219d3104c99a4cb21ce93fec11a8e7a
Nwobodo Ikenna <nikenna06@gmail.com>
2024-01-03 20:12:54 +0100

    Update the home page to display the last 5 tasks instead of all tasks.

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -25,23 +25,26 @@ function Home() {
         <div className="w-full md:w-10/12">
           <NewTask position="center" />
         </div>
-        <div className="w-full flex flex-col gap-4  items-end">
-          <div className="border border-[#CFCFCF} w-full p-4 rounded-xl shadow-inner h-max min-h-[50%] grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+        <div className="w-full flex bg-green-700 flex-col gap-4 h-3/5 items-end">
+          <div className="border border-[#CFCFCF} bg-red-700 w-full p-4 rounded-xl shadow-inner flex-1 min-h-[50%] grid grid-cols-1 place-items-center md:grid-cols-2 lg:grid-cols-3 gap-5">
             {Object.keys(tasks).length === 0 ? (
               <p className="font-medium tex-md text-opacity-70 text-neutral-500">
                 Nothing to see here yet
               </p>
             ) : (
-              Object.keys(tasks).map((id) => {
-                return (
-                  <Task
-                    id={id}
-                    title={tasks[id].title}
-                    time={tasks[id].date}
-                    status={tasks[id].status}
-                  />
-                );
-              })
+              Object.keys(tasks)
+                .slice(-5)
+                .reverse()
+                .map((id) => {
+                  return (
+                    <Task
+                      id={id}
+                      title={tasks[id].title}
+                      time={tasks[id].date}
+                      status={tasks[id].status}
+                    />
+                  );
+                })
             )}
           </div>
           <a
